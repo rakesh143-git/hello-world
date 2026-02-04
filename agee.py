6610fc3a7bd61bdacb7377f22fc69b8fb6a3a133
rakesh143-git <rakeshvh143@gmail.com>
2026-02-04 10:32:05 +0530

    day3

--- agee.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name:")
-
-age = int(input("enter your age:"))
-
-age = age + 2
-
-print("hey", name, "you will be", age, "years old in 2026!")
---
+++ data.py
@@ -0,0 +1,10 @@
+item_name = "Laptop"
+quantity = 4
+price = 2999.99
+in_stock = True
+
+print("Item:", item_name, ", Qty:", quantity, ", price:", price, ", Available:", in_stock)
+
+total_cost = quantity * price
+
+print("Total Cost:", total_cost)
