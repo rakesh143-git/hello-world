b6e076ae7597e57d7327a19284b0cb611474996b
rakesh143-git <rakeshvh143@gmail.com>
2026-02-06 11:54:15 +0530

    day5

---
+++ main.py
@@ -0,0 +1,8 @@
+from math_operations import power, average
+
+result_power = power(2, 10)
+numbers = [10, 20, 30, 40]
+result_average = average(numbers)
+
+print(f"2 to the power of 10 is: {result_power}")
+print(f"The average of {numbers} is: {result_average}")
